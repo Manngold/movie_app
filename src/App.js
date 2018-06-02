132002e92d000a4565e0da596b9ea44b9a569deb
Manngold <36980767+Manngold@users.noreply.github.com>
2018-06-02 21:57:19 +0900

    state practice

--- src/App.js
+++ src/App.js
@@ -2,32 +2,55 @@ import React, { Component } from "react";
 import "./App.css";
 import Movie from './Movie';
 
-const movies = [
-  {
-   title: "Matirx",
-   poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIVxLB1FVL2POzHuv2R6TbhXzONdxLmyJUqKLuUyDZRcY06AtX" 
-  },
-  {
-    title: "Full Metal Jacket",
-    poster:"https://images-na.ssl-images-amazon.com/images/I/71qDKzqJZrL._SL1101_.jpg"
-  },
-  {
-    title: "Oldboy",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU1tTmiH4_DdTSbDOsrrnANifLOoVpIOfeGNRiJrYu2WkMPAumzA"
-  },
-  {
-    title: "Star Wars",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqE44O4JQKJ23DYL8VF-3t5dofC7BIZ6VOJxSmTtylgfMbGrdj"
-  }
 
-]
 class App extends Component {
+
+  //Render: componentWillMount => render => componentDidMount
+  //Update: componentWillReceiveProps() => shouldComponentUpdate() => componentWillUpdate() => render() =>componentDidUpdate()
+
+  state = { }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        movies: [
+  
+          {
+           title: "Matirx",
+           poster:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIVxLB1FVL2POzHuv2R6TbhXzONdxLmyJUqKLuUyDZRcY06AtX" 
+          },
+          {
+            title: "Full Metal Jacket",
+            poster:"https://images-na.ssl-images-amazon.com/images/I/71qDKzqJZrL._SL1101_.jpg"
+          },
+          {
+            title: "Oldboy",
+            poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU1tTmiH4_DdTSbDOsrrnANifLOoVpIOfeGNRiJrYu2WkMPAumzA"
+          },
+          {
+            title: "Star Wars",
+            poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqE44O4JQKJ23DYL8VF-3t5dofC7BIZ6VOJxSmTtylgfMbGrdj"
+          },
+          {
+            title:'Transpoting',
+            poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp7n7rhKsAmOeHdlDK-xozOLkO2kIN0WedJs_SHjCh_Coug_ypBw"
+          }
+        
+        ]
+      })
+    },5000)
+  }
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+     })
+     return movies
+  }
   render() {
+    
     return (
       <div className="App">
-       {movies.map(movie => {
-        return <Movie title={movie.title} poster={movie.poster} />
-       })}
+       {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
       
     );
--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'; //import Componet from react
+import PropTypes from 'prop-types';
 import './Movie.css';
 
 class Movie extends Component{
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired
+    }//check prop of title, poster
+
+
     render(){
         return(
             <div>
@@ -13,9 +21,14 @@ class Movie extends Component{
 
 }
 class MoviePoster extends Component{
+
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
+
     render(){
         return(
-            <img src={this.props.poster} />
+            <img src={this.props.poster} alt="Not Found error"/>
         )
     }
 }
