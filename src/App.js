bde0df45f1dce5a1531510aac02b6310f486ab22
Manngold <36980767+Manngold@users.noreply.github.com>
2018-06-03 21:35:52 +0900

    movie_app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 
   //Render: componentWillMount => render => componentDidMount
   //Update: componentWillReceiveProps() => shouldComponentUpdate() => componentWillUpdate() => render() =>componentDidUpdate()
-  state={}
+  state={};
   
   componentDidMount() {
     this._getMovies();
@@ -15,31 +15,38 @@ class App extends Component {
   
   _renderMovies = () => {//Calling movie list
     const movies = this.state.movies.map((movie) => {
-      console.log(movie)
-      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id}/>
-     })
-     return movies
-  }
+      return (
+      <Movie 
+      title={movie.title_english}
+      poster={movie.medium_cover_image}
+      key={movie.id} 
+      genres={movie.genres}
+      synopsis={movie.synopsis}
+      />
+    );
+     });
+     return movies;
+  };
 
   _getMovies = async () => {
     const movies = await this._callApi()
     this.setState({
       movies
-    })
-  }
+    });
+  };
 
   _callApi = () => {
     return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
-   .then(res => res.json())
-   .then(json => json.data.movies)
-   .catch(err => console.log(err))
-  }
+    .then(res => res.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err));
+  };
 
   render() {
-    
+    const { movies } = this.state;
     return (
-      <div className="App">
-       {this.state.movies ? this._renderMovies() : 'Loading'}
+      <div className={movies ? "App" : "App--loading"}>
+       {movies ? this._renderMovies() : 'Loading'}
       </div>
       
     );
--- src/Movie.js
+++ src/Movie.js
@@ -1,34 +1,55 @@
 import React, { Component } from 'react'; //import Componet from react
 import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis';
 import './Movie.css';
 
 class Movie extends Component{
 
     static propTypes = {
         title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
+        poster: PropTypes.string.isRequired,
+        genres: PropTypes.array.isRequired,
+        synopsis: PropTypes.string.isRequired
     }//check prop of title, poster
 
 
     render(){
         return(
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
+            <div className="Movie">
+                <div className="Movie__Column">
+                    <MoviePoster poster={this.props.poster} alt={this.props.title}/>
+                </div>
+                <div className="Movie__Column">
+                        <h1>{this.props.title}</h1>
+                        <div className="Movie__Genres">
+                            {this.props.genres+" "}
+                        <div className="Movie__Synopsis">
+                            <LinesEllipsis
+                            text={this.props.synopsis}
+                            maxLine='3'
+                            ellipsis='...'
+                            trimRight
+                            basedOn='letters'
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }//Component alway need to show something
 
 }
+
 class MoviePoster extends Component{
 
     static propTypes = {
-        poster: PropTypes.string.isRequired
+        poster: PropTypes.string.isRequired,
+        alt: PropTypes.string.isRequired
     }
 
     render(){
         return(
-            <img src={this.props.poster} alt="Not Found error"/>
+            <img src={this.props.poster} alt={this.props.alt} title={this.props.alt} className="Movie__Poster"/>//when hover movie title appear
         )
     }
 }
